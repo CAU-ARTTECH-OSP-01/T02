1f932ee6277a43f5943b511013b66ce4e41603e9
Jaeyeon <81911992+Luna-Jaeyeon@users.noreply.github.com>
2021-12-02 22:11:25 +0900

    java script file

---
+++ data.js
@@ -0,0 +1,8 @@
+const storyList = [
+  "./image/image-03.png", "./image/image-04.png",
+  "./image/image-05.png", "./image/image-06.png",
+  "./image/image-07.png", "./image/image-08.png",
+  "./image/image-09.png", "./image/image-10.png",
+  "./image/image-11.png", "./image/image-12.png",
+  "./image/image-13.png", "./image/image-14.png"
+]
